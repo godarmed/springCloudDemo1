aa94d58be257278ce7a31afb8b270f01902f4e98
zengzhiyuan <2570613257@qq.com>
2019-09-01 22:26:30 +0800

    Initial commit

---
+++ src/main/java/com/springcloud/parentdemo/ParentDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.parentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParentDemoApplication.class, args);
+    }
+
+}
